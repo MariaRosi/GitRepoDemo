bc658a6b3ec8b8dfcda3939b56c5ce7df63abf30
MariaRosiGit <maria.rosi2008@gmail.com>
2018-04-05 19:03:49 +0100

    This is the first commit*

---
+++ src/main/java/com/qa/utils/UtilityClass.java
@@ -0,0 +1,8 @@
+package com.qa.utils;
+
+public class UtilityClass {
+	public void utilityMethod() {
+		System.out.println("Utility Method");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/HomeTest.java
@@ -0,0 +1,11 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+	@Test
+	public void loginTest() {
+		System.out.println("Login Test");
+	}
+
+}
